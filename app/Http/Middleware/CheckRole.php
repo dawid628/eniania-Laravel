ea99ffef11e303cf8aec3085a12b0d69b37f3dc7
dawidmetelski <dawid628@onet.eu>
2022-12-28 12:17:17 +0100

    roles middleware

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -16,6 +16,16 @@ class CheckRole
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if ($request->user() === null){
+            return redirect('/login');
+        }
+
+        $actions = $request->route()->getAction();
+        $roles = isset($actions['roles']) ? $actions['roles'] : null;
+
+        if($request->user()->hasAnyRole($roles)){
+            return $next($request);
+        }
+        return redirect('/');
     }
 }
